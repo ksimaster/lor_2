0cd507f88c09ee0723f532aecae76c0bef4008de
ivan.mochalov <ivan.mochalov@aspose.com>
2022-07-31 13:33:00 +0300

    Perlin noise + cam position

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -21,10 +21,15 @@ public class TileMap : MonoBehaviour
     }
 
     public void SetTiles() {
-        Vector2Int size = new Vector2Int(8, 8);
+        Vector2Int size = new Vector2Int(32, 32);
+        float seed = Time.realtimeSinceStartup;
          for (int x = 0; x < size.x; x++) {
              for (int y = 0; y < size.y; y++) {
-                tilemap.SetTile(new Vector3Int(x, y, 0), new PrefabTile(myPrefab, x, y));
+                float sample = Mathf.PerlinNoise(seed *999 + (float)x/4, (float)y/4);
+                if (sample > 0.3)
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), new PrefabTile(myPrefab, x, y));
+                }
              }
          }
      }
