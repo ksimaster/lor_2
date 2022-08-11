e69a217d2a2fda0f743a0903b4447276d96b87c0
IIEC-kycaka <34373675+IIEC-kycaka@users.noreply.github.com>
2022-08-11 17:21:13 +0300

    For MM and Quality

---
+++ Assets/Scripts/Menu/Settings.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public Dropdown qualityDropdown;
+
+    void Start()
+    {
+    }
+
+
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("QualitySettingPreference", qualityDropdown.value);
+    }
+
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("QualitySettingPreference"))
+            qualityDropdown.value = PlayerPrefs.GetInt("QualitySettingPreference");
+        else
+            qualityDropdown.value = 6;
+    }
+}
